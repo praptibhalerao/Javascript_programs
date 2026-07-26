23df1e3683d7a146b38e890e5abb84f2395781fe
praptibhalerao <bhaleraoprapti2005@gmail.com>
2026-07-27 00:24:24 +0530

    conversionOperation

--- demo.js
+++ demo.js
@@ -1,2 +1,2 @@
 console.log("hello world");
-console.log("  I am currently study in mgm University");
+console.log("I am currently study in mgm University");
---
+++ variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "bhaleraoprapti2005@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountStatus;
+
+// accountId = 2 not allow
+
+
+
+console.log(accountId);
+
+/*Prefer not to use var
+because of issue in block scope and functional Scope*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountStatus])
